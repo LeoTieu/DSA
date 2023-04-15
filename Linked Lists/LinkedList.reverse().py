d7a23e9d96c98f15d4d4e3250b5d399f9b85331f
MayoLayo <l.t05@outlook.com>
2023-04-15 16:29:18 +0200

    Reverse Linked List
    Created a function to reverse a linked list

---
+++ Linked Lists/LinkedList.reverse().py
@@ -0,0 +1,57 @@
+# Write a function to reverse the elements in the linked list.
+# # Function has a time complexity of O(N)
+
+class node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class linked_list:
+    def __init__(self, lst=None):
+        self.head = node()
+        if lst is not None:
+            for elem in lst:
+                self.append(elem)
+    
+
+    def reverse(self):
+        '''
+        Reverses elements within the linked list
+        '''
+        new_node = node()
+        cur_node = self.head.next
+        past_node = None
+        second_last_node = None
+        while cur_node is not None:
+            second_last_node = past_node
+            past_node = cur_node
+            cur_node = cur_node.next
+            past_node.next = second_last_node
+        self.head.next = past_node
+
+
+    def display(self):
+        '''
+        Displays elements in a list format
+        '''
+        elements = []
+        cur_node = self.head
+        while cur_node.next is not None:
+            cur_node = cur_node.next
+            elements.append(cur_node.data)
+        print(elements)
+
+    def append(self, data):
+        '''
+        Appends data to end of list
+        '''
+        new_node = node(data)
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = new_node
+
+if __name__ == '__main__':
+    MyLinkedList = linked_list(["f", "d", "g", "h", "o"])
+    MyLinkedList.reverse()
+    MyLinkedList.display()
