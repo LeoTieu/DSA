03fe0175add68d83ea0bcb5c9fea0452478e2c40
MayoLayo <l.t05@outlook.com>
2023-05-16 18:57:48 +0200

    Optimized function
    The middle is most likely larger or smaller than middle so having it return middle as an else statement will optimize is a small bit

--- Searching Functions/BinarySearch.py
+++ Searching Functions/BinarySearch.py
@@ -2,17 +2,17 @@ def binary_search(list, target):
     '''
     Searches a target and returns target's index.
     Returns -1 if target was not found.
-    Implemented in logarithmic time.
+    Time complexity is O(log n) where n is the list length.
     '''
     left = 0
-    right = len(list)-1
+    right = len(list) - 1
     while left <= right:
         middle = (left + right) // 2
-        if list[middle] == target:
-            return middle
         if list[middle] > target:
             right = middle - 1
-        else:
+        elif list[middle] < target:
             left = middle + 1
+        else:
+            return middle
     return -1
 
