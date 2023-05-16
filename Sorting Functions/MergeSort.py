c48c5bf1bb5252263c3b55bc9e48ae63de1dcef0
MayoLayo <l.t05@outlook.com>
2023-05-16 19:29:57 +0200

    Made more readable code.
    Added comments and more readable code. Usually not a fan of comment but since I'm doing it to learn. Writing comments might be better.
    
    This will probably work less efficient than using negative pointers, but it is a lot more readable.

--- Sorting Functions/MergeSort.py
+++ Sorting Functions/MergeSort.py
@@ -1,23 +1,32 @@
 # Personal comments:
+# hmu if I do something "wrong"
+
 # Why not pop(0)?
 # Shifting elements take o(n)
 # Emptying the whole list will be quadratic in time.
 
 def Merge(l1,l2):
-    '''Merging 2 sorted lists'''
+    '''Merges 2 sorted lists'''
+    # Result list. Will be returned.
     l3 = []
-    # Points to the first element of list
-    pointer_1 = len(l1) * -1
-    pointer_2 = len(l2) * -1
-    while pointer_1 != 0 and pointer_2 != 0:
+    # Points to the first element of list.
+    pointer_1 = 0
+    pointer_2 = 0 
+    while pointer_1 < len(l1) and pointer_2 < len(l2):
+        # Checks which element is smallest.
         if l1[pointer_1] <= l2[pointer_2]:
+            # Adds element where pointer is located at.
             l3.append(l1[pointer_1])
+            # Points to next element.
             pointer_1 += 1
         else:
             l3.append(l2[pointer_2])
             pointer_2 += 1
-    if pointer_1 == 0:
+    # if pointer_1 outside l1. Add the rest of l2.
+    # And vice versa ofc.
+    if pointer_1 == len(l1):
         l3 += l2[pointer_2:]
     else:
         l3 += l1[pointer_1:]
     return l3
+
