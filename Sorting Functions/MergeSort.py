ba157a7ea63ebb35a7e40d744857ec00d1853538
MayoLayo <l.t05@outlook.com>
2023-05-20 22:00:56 +0200

    Merged merge with mergesort
    Removed merge function and added it to the merge_sort function. Instead of creating a new list, I emptied the input list to hopefully use less memory.

--- Sorting Functions/MergeSort.py
+++ Sorting Functions/MergeSort.py
@@ -25,38 +25,30 @@ def merge_sort(List):
         Right_List = merge_sort(Right_List)
 
         # Sort left + right list
-        return merge(Left_List, Right_List)
+        List = []
+        pointer_1 = 0
+        pointer_2 = 0
+        while pointer_1 < len(Left_List) and pointer_2 < len(Right_List):
+            # Checks which element is bigger
+            if Left_List[pointer_1] <= Right_List[pointer_2]:
+                # Adds element and moves pointer
+                List.append(Left_List[pointer_1])
+                pointer_1 += 1
+            else:
+                List.append(Right_List[pointer_2])
+                pointer_2 += 1
+        # if pointer_1 outside l1. Add the rest of l2.
+        # And vice versa ofc.
+        if pointer_1 == len(Left_List):
+            List += Right_List[pointer_2:]
+        else:
+            List += Left_List[pointer_1:]
+        return List            
     else:
         return List
-        
 
-
-def merge(l1,l2):
-    '''Merges 2 sorted lists'''
-    # Result list. Will be returned.
-    l3 = []
-    # Points to the first element of list.
-    pointer_1 = 0
-    pointer_2 = 0 
-    while pointer_1 < len(l1) and pointer_2 < len(l2):
-        # Checks which element is smallest.
-        if l1[pointer_1] <= l2[pointer_2]:
-            # Adds element where pointer is located at.
-            l3.append(l1[pointer_1])
-            pointer_1 += 1
-        else:
-            l3.append(l2[pointer_2])
-            pointer_2 += 1
-
-    # if pointer_1 outside l1. Add the rest of l2.
-    # And vice versa ofc.
-    if pointer_1 == len(l1):
-        l3 += l2[pointer_2:]
-    else:
-        l3 += l1[pointer_1:]
-    return l3
 
 if __name__ == '__main__':
     arr = [3, 20, 7, 9, 13, 14, 10, 2, 5, 1, 8, 18, 4, 11, 15, 6, 16, 19, 12, 17]
-    print(merge(arr))
+    print(merge_sort(arr))
 
