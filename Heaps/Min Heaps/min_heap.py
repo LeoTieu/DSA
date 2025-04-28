2c2912910ffe4b76e26c4549afb1914d2ba8300b
MayoLayo <l.t05@outlook.com>
2025-04-28 14:17:06 +0200

    Made a working min heap sort function that takes a heap and makes it a min heap

--- Heaps/Min Heaps/min_heap.py
+++ Heaps/Min Heaps/min_heap.py
@@ -4,36 +4,39 @@
 
 
 class MinHeap:
-    def __init__(self, heap: list = []):
+    def __init__(self, heap: list = None):
+        if heap == None:
+            heap = []
         self.min_heap = heap
     
 
-def min_heap_sort(heap: list, current_node_index: int = 0):
-    '''
-    Trying to implement a version of min heap
-    '''
+def min_heapify(heap: list, current_node_index: int = 0):
     current_node = heap[current_node_index]
     left_node_index = current_node_index * 2 + 1
     right_node_index = current_node_index * 2 + 2    
 
 
     if left_node_index < len(heap):
-        left_node = heap[left_node_index]
-        if left_node < current_node:
-            heap[left_node_index], heap[current_node_index] = current_node, left_node
-            min_heap_sort(heap, left_node_index)
+        if heap[left_node_index] < heap[current_node_index]:
+            heap[left_node_index], heap[current_node_index] = heap[current_node_index], heap[left_node_index]
+            min_heapify(heap, left_node_index)
 
 
     if right_node_index < len(heap):
-        right_node = heap[right_node_index]
-        if right_node < current_node:
-            heap[right_node_index], heap[current_node_index] = current_node, right_node
-            min_heap_sort(heap, right_node_index)
-    
+        if heap[right_node_index] < heap[current_node_index]:
+            heap[right_node_index], heap[current_node_index] = heap[current_node_index], heap[right_node_index]
+            min_heapify(heap, right_node_index)
     return
+
+
+def min_heap_sort(heap: list):
+    for i in range(len(heap) // 2 - 1, -1, -1):
+        print(i)
+        min_heapify(heap, i)
 
 
 if __name__ == '__main__':
     my_heap = [9, 4, 7, 1, -2, 6, 5]
+    # my_heap = [7,6,5,4,3,2,1]
     min_heap_sort(my_heap)
     print("sorted heap = ", my_heap)
