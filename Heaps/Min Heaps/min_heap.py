e878162a03ca5d5fa092034540656fc3d697026c
MayoLayo <l.t05@outlook.com>
2025-04-28 13:35:15 +0200

    Runnable but not working min heap sort

--- Heaps/Min Heaps/min_heap.py
+++ Heaps/Min Heaps/min_heap.py
@@ -9,24 +9,31 @@ class MinHeap:
     
 
 def min_heap_sort(heap: list, current_node_index: int = 0):
-    current_node_index += 1
-    left_node_index = current_node_index * 2 - 1
-    right_node_index = current_node_index * 2
-
+    '''
+    Trying to implement a version of min heap
+    '''
     current_node = heap[current_node_index]
-    left_node = heap[left_node_index]
-    right_node = heap[right_node_index]
+    left_node_index = current_node_index * 2 + 1
+    right_node_index = current_node_index * 2 + 2    
 
-    if max(current_node, left_node, right_node) == current_node:
-        min_heap_sort(heap, left_node)
-        min_heap_sort(heap, right_node)
-        return
+
+    if left_node_index < len(heap):
+        left_node = heap[left_node_index]
+        if left_node < current_node:
+            heap[left_node_index], heap[current_node_index] = current_node, left_node
+            min_heap_sort(heap, left_node_index)
+
+
+    if right_node_index < len(heap):
+        right_node = heap[right_node_index]
+        if right_node < current_node:
+            heap[right_node_index], heap[current_node_index] = current_node, right_node
+            min_heap_sort(heap, right_node_index)
     
-    if right_node >= left_node:
-        current_node, right_node = right_node, current_node
-    else:
-        current_node, left_node = left_node, current_node
-    
-    min_heap_sort(heap, left_node)
-    min_heap_sort(heap, right_node)
     return
+
+
+if __name__ == '__main__':
+    my_heap = [9, 4, 7, 1, -2, 6, 5]
+    min_heap_sort(my_heap)
+    print("sorted heap = ", my_heap)
--- Heaps/Min Heaps/min_heap_test.py
+++ Heaps/Min Heaps/min_heap_test.py
@@ -3,17 +3,18 @@ from min_heap import min_heap_sort
 
 class TestMinHeap(unittest.TestCase):
     def test_min_heap_property(self):
-        input_list = [9, 4, 7, 1, -2, 6, 5]
-        heap = min_heap_sort(input_list)
+        heap = [9, 4, 7, 1, -2, 6, 5]
+        min_heap_sort(heap)
+        print("sorted heap = ", heap)
 
-        for i in range(len(heap.min_heap)):
+        for i in range(len(heap)):
             left = 2 * i + 1
             right = 2 * i + 2
 
-            if left < len(heap.min_heap):
-                self.assertLessEqual(heap.min_heap[i], heap.min_heap[left])
-            if right < len(heap.min_heap):
-                self.assertLessEqual(heap.min_heap[i], heap.min_heap[right])
+            if left < len(heap):
+                self.assertLessEqual(heap[i], heap[left])
+            if right < len(heap):
+                self.assertLessEqual(heap[i], heap[right])
 
 if __name__ == "__main__":
     unittest.main()
