13be58c593630edc1e9dca7f52ee8bcdb4520b82
MayoLayo <l.t05@outlook.com>
2025-04-28 14:16:01 +0200

    Made the test do 10 random heaps with size of 30 to 50, and numbers from 1 to 50

--- Heaps/Min Heaps/min_heap_test.py
+++ Heaps/Min Heaps/min_heap_test.py
@@ -1,20 +1,23 @@
 import unittest
+from random import randint
+
 from min_heap import min_heap_sort
+
 
 class TestMinHeap(unittest.TestCase):
     def test_min_heap_property(self):
-        heap = [9, 4, 7, 1, -2, 6, 5]
-        min_heap_sort(heap)
-        print("sorted heap = ", heap)
+        for i in range(10):
+            heap = [randint(1,50) for _ in range(30)]
+            min_heap_sort(heap)
 
-        for i in range(len(heap)):
-            left = 2 * i + 1
-            right = 2 * i + 2
+            for i in range(len(heap)):
+                left = 2 * i + 1
+                right = 2 * i + 2
 
-            if left < len(heap):
-                self.assertLessEqual(heap[i], heap[left])
-            if right < len(heap):
-                self.assertLessEqual(heap[i], heap[right])
+                if left < len(heap):
+                    self.assertLessEqual(heap[i], heap[left])
+                if right < len(heap):
+                    self.assertLessEqual(heap[i], heap[right])
 
 if __name__ == "__main__":
     unittest.main()
