c60a0de0eda8748ed9138511775c4295e39bd561
MayoLayo <l.t05@outlook.com>
2025-04-29 09:44:42 +0200

    Added insertion function to min heap class

--- Heaps/Min Heaps/min_heap.py
+++ Heaps/Min Heaps/min_heap.py
@@ -1,15 +1,41 @@
-# Left child = current_node * 2 
-# Right child = current_node * 2 + 1
-# Works if first node is counted as 1
+# Current min_heap
 
 
 class MinHeap:
-    '''Takes a list and converts it into a min heap'''    
+    '''Min heap datastructure meant soley for learning'''    
     def __init__(self, heap: list = None):
         if heap == None:
             heap = []
         self.min_heap = heap
         convert_to_min_heap(self.min_heap)
+
+    
+    def get_min(self) -> int:
+        '''Gets the minimum value in the min heap'''
+        return self.heap[0]
+    
+
+    def extract_min(self) -> int:
+        '''
+        Gets the minimum value in the min heap and removes it
+        Keeps the min heap structure
+        '''
+        pass
+
+
+    def insert(self, number: int) -> None:
+        self.min_heap.append(int)
+        current_index = len(self.min_heap)
+        
+        def bottom_up_heapify(self, child_index):
+            if child_index == 0:
+                return
+
+            parent_index = (child_index-1)//2
+            if self.heap[child_index] < self.heap[parent_index]:
+                self.heap[child_index], self.heap[parent_index] = self.heap[parent_index], self.heap[child_index]
+                bottom_up_heapify(parent_index)
+            return
 
 
 def min_heapify(heap: list, current_node_index: int = 0):
