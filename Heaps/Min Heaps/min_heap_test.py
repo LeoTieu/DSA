744ca9959b8dbd2ec4deae50ce9accdbfc378e64
MayoLayo <l.t05@outlook.com>
2025-04-29 09:45:24 +0200

    Implemented object creation test and changed names and structure

--- Heaps/Min Heaps/min_heap_test.py
+++ Heaps/Min Heaps/min_heap_test.py
@@ -1,23 +1,35 @@
 import unittest
 from random import randint
-
-from min_heap import convert_to_min_heap
+from min_heap import convert_to_min_heap, MinHeap
 
 
 class TestMinHeap(unittest.TestCase):
-    def test_min_heap_property(self):
+    def test_convert_to_min_heap(self):
         for i in range(10):
             heap = [randint(1,50) for _ in range(randint(30,50))]
             convert_to_min_heap(heap)
+            self._assert_is_min_heap(heap)
 
-            for i in range(len(heap)):
-                left = 2 * i + 1
-                right = 2 * i + 2
+    
+    
 
-                if left < len(heap):
-                    self.assertLessEqual(heap[i], heap[left])
-                if right < len(heap):
-                    self.assertLessEqual(heap[i], heap[right])
+    def test_creation_of_min_heap(self):
+        for i in range(10):
+            heap = [randint(1,50) for _ in range(randint(30,50))]
+            min_heap = MinHeap(heap)
+            self._assert_is_min_heap(min_heap.min_heap)
+
+
+    def _assert_is_min_heap(self, heap):
+        for i in range(len(heap)):
+            left = 2 * i + 1
+            right = 2 * i + 2
+
+            if left < len(heap):
+                self.assertLessEqual(heap[i], heap[left])
+            if right < len(heap):
+                self.assertLessEqual(heap[i], heap[right])
+
 
 if __name__ == "__main__":
     unittest.main()
